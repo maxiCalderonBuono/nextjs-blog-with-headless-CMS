7ccde73cbfc65217d381dc8e4203675a073276bb
Maxi <calderonmaxi@outlook.com>
2023-06-23 00:14:42 +0200

    fix: solved dynamic classes with tailwind

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -9,7 +9,6 @@ interface PostListProps {
 
 const Grid = ({ posts }: PostListProps) => {
 
-  const gridSize = posts.length > 1 ? 2 : 1
   const textSize = posts.length > 1 ? "text-2xl" : "text-2xl md:text-4xl"
 
 
@@ -17,7 +16,7 @@ const Grid = ({ posts }: PostListProps) => {
   return (
     <section className="my-10 md:my-20">
       <h1 className="py-5 mb-5 text-5xl font-extrabold text-center lg:text-6xl md:text-left">DESTACADOS</h1>
-      <div className={`grid gap-10 grid-cols-1 md:grid-cols-${gridSize}`}>
+      <div className={`grid gap-10 grid-cols-1 ${posts.length > 1 ? "md:grid-cols-2" : "md:grid-cols-1"}`}>
         {posts.map((post, index) => (
           <Card key={post.sys.id} attributes={post.fields} textSize={textSize}/>
         ))}
