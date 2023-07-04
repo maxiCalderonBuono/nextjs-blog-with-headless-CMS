ee09dc12f4f958079c3022c6457c783b33c72625
Maxi <calderonmaxi@outlook.com>
2023-07-04 22:07:53 +0200

    fix: fixed mobile menu dimensions

--- components/SlideOver.tsx
+++ components/SlideOver.tsx
@@ -28,7 +28,7 @@ const SlideOver = ({ posts }: SliderProps) => {
   return (
     <div className="flex items-center md:hidden">
       <button type="button" onClick={() => setOpen(true)} aria-expanded={open} aria-controls="slider-over-menu" aria-haspopup="true" className="p-2 rounded-md focus:outline-indigo-500 focus:outline focus:outline-2" aria-label="Menú mobile">
-        <div className="relative flex items-center hover:scale-105 hover:opacity-90 active:scale-95" >
+        <div className="relative flex items-center hover:scale-105 hover:opacity-90 active:scale-95 w-[26px] h-[26px]" >
           <ClosedMenu />
         </div>
       </button>
