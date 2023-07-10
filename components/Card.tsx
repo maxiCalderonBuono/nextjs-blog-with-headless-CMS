2a01210ae544727dc84b2e2de612bef7dbe3e0bf
Maxi <calderonmaxi@outlook.com>
2023-07-10 22:52:50 +0200

    fix: changed sizes to 120px

--- components/Card.tsx
+++ components/Card.tsx
@@ -25,7 +25,7 @@ export const Card = ({ attributes, textSize }: ArticleProps) => {
                   src={`https:${attributes.image.fields.file.url}`}
                   alt={attributes.title}
                   fill
-                  sizes="(max-width: 768px) 180px, 50vw"
+                  sizes="(max-width: 768px) 120px, 33vw"
                   priority
                   className="object-cover rounded-md shadow-lg"
                 />
