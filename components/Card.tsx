701b39e34b64d494b6d457e91f3c77f70a066fc6
Maxi <calderonmaxi@outlook.com>
2023-07-10 22:14:03 +0200

    fix:testing sizes

--- components/Card.tsx
+++ components/Card.tsx
@@ -25,7 +25,7 @@ export const Card = ({ attributes, textSize }: ArticleProps) => {
                   src={`https:${attributes.image.fields.file.url}`}
                   alt={attributes.title}
                   fill
-                  sizes="50vw"
+                  sizes="(max-width: 768px) 320px, 50vw"
                   priority
                   className="object-cover rounded-md shadow-lg"
                 />
